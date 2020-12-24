8477b4530c681e358dee7d7e820a0a24fdd1448c
Dacker Daw <dacker.daw@gmail.com>
2020-12-24 21:07:24 +0700

    tidied up  youtube_xml_feed.py

--- streamline/display/controllers/youtube_xml_feed.py
+++ streamline/display/controllers/youtube_xml_feed.py
@@ -1,11 +1,9 @@
 from aiohttp import ClientSession, TCPConnector
 import asyncio
-# import pypeln as pl
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 from asgiref.sync import sync_to_async
 
-from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 
 from display.models.channel import Channel
@@ -23,42 +21,6 @@ def fetchChannelXML(channelId):
 
     return (currChannelId, currChannelName)
 
-
-# doesn't work on windows
-# async def fetchXMLAsync():
-
-#     async with ClientSession(connector=TCPConnector(limit=0)) as session:
-
-#         async def fetch(url): # if only i knew how to use pub/sub, too bad
-#             async with session.get(url) as response:
-#                 chunk = await response.read()
-#                 tree = ET.ElementTree(ET.fromstring(chunk))
-#                 root = tree.getroot()
-#                 ns = '{http://www.w3.org/2005/Atom}'
-#                 uncrawledVideoIds = []
-
-#                 for entry in tree.iter(ns + 'entry'):
-#                     currVideoId = entry[1].text
-#                     try:
-#                         results = await sync_to_async(Video.objects.get, thread_sensitive=True)(pk=currVideoId)
-#                         # print('video found in db')
-#                     except ObjectDoesNotExist:
-#                         # fetch the video metadata with youtube api
-#                         # print('uncrawled video found: ' + currVideoId)
-#                         uncrawledVideoIds.append(currVideoId)
-#                 return uncrawledVideoIds
-
-#         channels = await sync_to_async(Channel.objects.all)()
-#         urls = [
-#             'https://www.youtube.com/feeds/videos.xml?channel_id=%s' % channel.channelId
-#             for channel in channels
-#         ]
-
-#         stage = await pl.task.map(fetch, urls, workers=5) # 5 workers seems to be the safest
-#         data = list(stage)
-
-#         return data
-
 async def fetch(url, session): # if only i knew how to use pub/sub, too bad
     async with session.get(url) as response:
         chunk = await response.read()
@@ -69,12 +31,11 @@ async def fetch(url, session): # if only i knew how to use pub/sub, too bad
 
         for entry in tree.iter(ns + 'entry'):
             currVideoId = entry[1].text
+            # Check if the video already exists in our database
             try:
                 results = await sync_to_async(Video.objects.get, thread_sensitive=True)(pk=currVideoId)
-                # print('video found in db')
             except ObjectDoesNotExist:
-                # fetch the video metadata with youtube api
-                # print('uncrawled video found: ' + currVideoId)
+                # collect all the uncrawled videos
                 uncrawledVideoIds.append(currVideoId)
         return uncrawledVideoIds
 
@@ -87,7 +48,7 @@ async def fetchXMLAsync():
         for channel in channels
     ]
     tasks = []
-    async with ClientSession(connector=TCPConnector(limit=10)) as session:
+    async with ClientSession(connector=TCPConnector(limit=5)) as session: # 5 seems to be the safest for my 3rd world country-tier internet
 
         for url in urls:
             task = asyncio.create_task(fetch(url, session))
